a05cf53b3b1dec96b320f8ebad46d41666a4dfea
pranaysankineni <pranayrao475@gmail.com>
2021-08-31 14:16:55 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser ='Lion King';
+const welcomeMessage =`Welcome to Flatbook, '${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
